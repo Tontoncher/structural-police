928c01d328426418ffe243e435260dfcff2041c3
Anton Semenov <an.semenov@tinkoff.ru>
2022-09-06 17:42:54 +0300

    Delete index.ts from src/constants

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export const defaultErrorMessages = {
-    importDisallow: ({ absoluteImportPath }) => `Not allowed to import from "${absoluteImportPath}"`,
-    missingFile: ({ schemaPath, missingNode }) => `The file is not described in the schema. In "${schemaPath}" expected a node "${missingNode}"`,
-    missingRules: ({ schemaPath }) => `There is no set of rules in the "${schemaPath}"`,
-};
